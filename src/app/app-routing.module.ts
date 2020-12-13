a8afee4a351a6778259a740781576e48c6ad92e2
Renovatio89 <lachpawel89@gmail.com>
2020-12-13 14:47:28 +0100

    add pages home and offer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {HomePageComponent} from "./home-page/home-page.component";
-import {OfertPageComponent} from "./ofert-page/ofert-page.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {OfertPageComponent} from "./pages/ofert-page/ofert-page.component";
+import {ErrorComponent} from "./modules/error/error.component";
 
 
 const routes: Routes = [
-  { path: "home", component: HomePageComponent },
-  { path: "offer", component: OfertPageComponent }
+  {path: "home", component: HomePageComponent},
+  {path: "offer", component: OfertPageComponent},
+  {path: "", component: HomePageComponent},
+  {path: "**", component: ErrorComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
