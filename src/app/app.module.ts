15e37c52f014d10a23146e4e52a626295054166d
Renovatio89 <lachpawel89@gmail.com>
2020-12-13 16:27:35 +0100

    offer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NavBarComponent} from './modules/nav-bar/nav-bar.component';
-import {PageContentComponent} from './modules/page-content/page-content.component';
-import {NavBarMenuItemComponent} from './modules/nav-bar-menu-item/nav-bar-menu-item.component';
-import {NavBarLogoComponent} from './modules/nav-bar-logo/nav-bar-logo.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {PageContentComponent} from './components/page-content/page-content.component';
+import {NavBarMenuItemComponent} from './components/nav-bar-menu-item/nav-bar-menu-item.component';
+import {NavBarLogoComponent} from './components/nav-bar-logo/nav-bar-logo.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {OfertPageComponent} from './pages/ofert-page/ofert-page.component';
-import { ErrorComponent } from './modules/error/error.component';
+import { ErrorComponent } from './components/error/error.component';
+import { OfferItemComponent } from './components/offer-item/offer-item.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ErrorComponent } from './modules/error/error.component';
     NavBarLogoComponent,
     HomePageComponent,
     OfertPageComponent,
-    ErrorComponent
+    ErrorComponent,
+    OfferItemComponent
   ],
   imports: [
     BrowserModule,
